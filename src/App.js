5da3837f08d77af5440aeb2bfc570d47b20176c5
Daniel Ko <danko56666@gmail.com>
2019-08-20 19:47:45 -0400

    setup home page/component and header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Home from "./pages/Home";
 
 function App() {
-    return <div />;
+    return (
+        <>
+            <Home />
+        </>
+    );
 }
 
 export default App;
