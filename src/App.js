0804bde7bb1ee8b4e7dd2504ad165282b1f3ed7a
Daniel Ko <danko56666@gmail.com>
2019-08-20 20:05:59 -0400

    initial global styles setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
+import GlobalStyles from "./components/globals/GlobalStyles";
 
 function App() {
     return (
         <>
+            <GlobalStyles />
             <Home />
         </>
     );
