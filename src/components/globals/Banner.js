8c6deb43a8358bed4bdea219ea8312ee6ed4ec85
Daniel Ko <danko56666@gmail.com>
2019-08-26 13:59:19 -0400

    banner component helper functions

--- src/components/globals/Banner.js
+++ src/components/globals/Banner.js
@@ -1,6 +1,12 @@
 import React from "react";
 import styled, { css, keyframes } from "styled-components";
-import { setColor } from "../../styles";
+import {
+    setColor,
+    setRem,
+    setLetterSpacing,
+    setBorder,
+    media
+} from "../../styles";
 const Banner = ({ className, title, text, children, greeting }) => {
     return (
         <div className={className}>
@@ -15,6 +21,35 @@ const Banner = ({ className, title, text, children, greeting }) => {
     );
 };
 const BannerWrapper = styled(Banner)`
-    color: red;
+  background: rgba(0, 0, 0, 0.7);
+  text-align: center;
+  padding: ${setRem(60)} ${setRem(32)};
+  ${setLetterSpacing(3)}
+  color: ${setColor.mainWhite};
+  h1 {
+    text-transform: capitalize;
+    font-size: ${setRem(48)};
+    color: ${setColor.primaryColor};
+    span {
+      color: ${setColor.mainWhite};
+    }
+  }
+  p {
+    width: 85%;
+    margin: 0 auto;
+  }
+  ${media.tablet` width: 70vw;
+    ${setBorder({ width: "6px", color: setColor.primaryColor })};
+    p {
+      width: 75%;
+    }`}
+   
+  
+  h1 {
+    /* animation */
+  }
+  .info {
+    /* animation */
+  }
 `;
 export default BannerWrapper;
--- src/styles.js
+++ src/styles.js
@@ -1,3 +1,5 @@
+import { css } from "styled-components";
+
 export const setColor = {
     primaryColor: "#af9a7d",
     mainWhite: "#fff",
@@ -12,16 +14,47 @@ export const setFont = {
 };
 
 export const setFlex = ({ x = "center", y = "center" } = {}) => {
-    return `
-        display: flex;
-        align-items:${y};
-        justify-content:${x}
-    `;
+    return `display:flex;align-items:${y};justify-content:${x}`;
 };
 
-export const setBackground = ({ img, color = "rgba(0,0,0,0)" } = {}) => {
-    return `
-        background: linear-gradient(${color}, ${color}),
-        url(${img}) center/cover fixed no-repeat;
-    `;
+export const setBackground = ({
+    img = "https://images.pexels.com/photos/1628086/pexels-photo-1628086.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    color = "rgba(0,0,0,0)"
+} = {}) => {
+    return `background: linear-gradient(${color}, ${color}),
+    url(${img}) center/cover fixed no-repeat`;
 };
+
+export const setRem = (number = 16) => {
+    return `${number / 16}rem`;
+};
+
+export const setLetterSpacing = (number = 2) => {
+    return `letter-spacing:${number}px`;
+};
+
+export const setBorder = ({
+    width = "2px",
+    style = "solid",
+    color = "black"
+} = {}) => {
+    return `border:${width} ${style} ${color}`;
+};
+
+const sizes = {
+    large: 1200,
+    desktop: 992,
+    tablet: 768,
+    phone: 576
+};
+
+// Iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((acc, label) => {
+    acc[label] = (...args) => css`
+        @media (min-width: ${sizes[label] / 16}em) {
+            ${css(...args)}
+        }
+    `;
+
+    return acc;
+}, {});
